503c601ccdbff5ef2fa0bf86558ec8baf088b6bf
Poorva Gokhale <poorvasgokhale@gmail.com>
2018-11-28 20:51:04 +0530

    upgrade ammonite version

--- project/Libs.scala
+++ project/Libs.scala
@@ -2,6 +2,6 @@ import sbt._
 
 object Libs {
   val scalaVersion = "2.12.7"
-  val Version = "1.2.1-16-48ce533"
+  val Version = "1.3.2"
   val `ammonite` = "com.lihaoyi" % "ammonite" % Version cross CrossVersion.full
 }
